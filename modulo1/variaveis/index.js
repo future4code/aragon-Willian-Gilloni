36b2125f4f09ebf53d7a7a3107551b93493320e8
WillianGilloni <willianggvet@gmail.com>
2022-03-21 15:56:11 -0300

    Acrecentar Exercícios de interpretação de código

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,52 @@
+/* exercicio 1:
+let a = 10
+let b = 10
+
+console.log(b) = 10
+
+b = 5
+console.log(a, b) = 10  5
+*/
+
+/* exercicio 2:
+a=10
+b=10
+c=10
+*/
+
+/* exercicio 3:let horasPorDia = prompt("Quantas horas você trabalha por dia?")
+let salarioDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${salarioDia/horasPorDia} por hora`)
+*/
+
+//Exercicio de escrita de codigo 1
+
+
+const nome = prompt("Qual é o seu nome?")
+
+const idade = prompt("Qual a sua idade?")
+
+console.log("Olá ", nome , "voce tem " ,idade , "anos.")
+
+//Exercicio de escrita de codigo 2
+
+const roupa = prompt("Você está usando roupa amarela?")
+console.log(roupa)
+
+const cabelo = prompt("Seu cabelo é verde?")
+console.log(cabelo)
+
+const corDePele = prompt("Sua cor de pele é morena?")
+console.log(corDaPele)
+
+// Exercicio de escrita de codigo 3
+
+let a = 10
+let b = 25
+let c = 10
+a = b
+b = c 
+
+console.log(a , b )
+
+
