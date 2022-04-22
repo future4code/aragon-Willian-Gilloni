f8832acb927b9558fe7f2dc3fd07a46669708676
WillianGilloni <willianggvet@gmail.com>
2022-04-22 18:24:18 -0300

    Acrescentar insta4 com renderização de listas

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -8,15 +8,102 @@ import valdomira from './img/valdomira.jpg'
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
 `
+// const lista = listaDePessoas.map((pessoa) => {
+//     return lista
+// })
+
+
 
 class App extends React.Component {
+
+  state = {
+    nomeInput: "",
+    nome2Input: "",
+    nome3Input: "",
+
+    listaDePessoas: [
+      {
+        nome: "Willian",
+        fotoUsuario: "https://picsum.photos/200/200?id=1",
+        fotoPost: "https://picsum.photos/200/200?id=1"
+      },
+      {
+        nome: "Valdomira",
+        fotoUsuario: 'https://picsum.photos/200/200?id=1',
+        fotoPost: 'https://picsum.photos/200/200?id=1'
+      },
+      {
+        nome: "Luiz",
+        fotoUsuario: 'https://picsum.photos/200/200?id=1',
+        fotoPost: 'https://picsum.photos/200/200?id=1'
+      },
+    ]
+  }
+  onChangeIdade = (event) => {
+    this.setState({ nomeInput: event.target.value })
+  }
+
+  onSubmitForm = (event) => {
+    event.preventDefault()
+
+    const novaListaDePessoas = [...this.state.listaDePessoas]
+    novaListaDePessoas.push({
+      nome: this.state.nomeInput,
+      nome2: this.state.nome2Input,
+      nome3: this.state.nome3Input,
+    })
+
+    this.setState({ listaDepessoas: novaListaDePessoas })
+  }
   render() {
     return (
+
       <MainContainer>
-        <Post
+        <form onSubmit={this.onSubmitForm}>
+          <label for="">
+            <input
+              name="Nome"
+              placeholder='Nome'
+              value={this.state.nomeInput}
+              onChange={this.onChangeNome}
+            />
+          </label>
+
+          <label>
+            <input
+              name="fotoUsuario"
+              placeholder='fotoUsuario'
+              value={this.state.nome2Input}
+              onChange={this.onChangeNome}
+            />
+          </label>
+          <label for="">
+            <input
+              name="fotoPost"
+              placeholder='fotoPost'
+              value={this.state.nomeInput}
+              onChange={this.onChangeNome}
+            />
+          </label>
+
+          <button>Adicionar</button>
+        </form>
+        {this.state.listaDePessoas.map((pessoa) => {
+          console.log(pessoa)
+          return <Post
+            nomeUsuario={pessoa.nome}
+            fotoUsuario={pessoa.fotoUsuario}
+            fotoPost={pessoa.fotoPost}
+          />
+        })}
+      </MainContainer>
+    );
+  }
+}
+{/* <Post
           nomeUsuario={'Willian'}
           fotoUsuario={will}
           fotoPost={will}
@@ -30,10 +117,9 @@ class App extends React.Component {
         nomeUsuario={'Luiz'}
         fotoUsuario={lui}
         fotoPost={lui}
-        />
-      </MainContainer>
-    );
-  }
-}
+        /> */}
+
+
+
 
 export default App;
