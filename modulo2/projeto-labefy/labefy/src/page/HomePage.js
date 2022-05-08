46f5b8c48662bdfb6449639c6dc37e569bf7fd76
WillianGilloni <willianggvet@gmail.com>
2022-05-08 19:08:02 -0300

    atualizando

--- modulo2/projeto-labefy/labefy/src/page/HomePage.js
+++ modulo2/projeto-labefy/labefy/src/page/HomePage.js
@@ -15,12 +15,16 @@ export default class HomePage extends React.Component {
         nome: "",
         autor: "",
         urlDaMusica: "",
+        newPlayListTrack:{}
     }
 
     componentDidMount() {
         this.getAllPlaylists()
     }
 
+    componentDidUpdate() {
+        this.getAllPlaylists()
+    }
     handleName = (event) => {
         this.setState({ nome: event.target.value })
     }
@@ -38,24 +42,24 @@ export default class HomePage extends React.Component {
             .catch((error) => console.log(error.response))
     }
 
-    // addTrackToPlaylist = () => {
-    //     const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/:playlistId/tracks"
-    //     const body = {
-    //         name: this.state.nome,
-    //         autor: this.state.autor,
-    //         url: this.state.urlDaMusica
-    //     }
-    //     axios.post(url, body, {
+    addTrackToPlaylist = () => {
+        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/:playlistId/tracks"
+        const body = {
+            name: this.state.nome,
+            autor: this.state.autor,
+            url: this.state.urlDaMusica
+        }
+        axios.post(url, body,header, {
 
-    //     })
-    //         .then((response) => {
-    //             alert("Musica cadastrada com sucesso")
-    //             this.setState({ nome: "", autor: "", urlDaMusica: "" })
-    //         })
-    //         .catch((error) => {
-    //             alert(error.response.data.message)
-    //         })
-    // }
+        })
+            .then((response) => {
+                alert("Musica cadastrada com sucesso")
+                this.setState({ nome: "", autor: "", urlDaMusica: "" })
+            })
+            .catch((error) => {
+                alert(error.response.data.message)
+            })
+    }
 
     render() {
 
@@ -68,28 +72,29 @@ export default class HomePage extends React.Component {
         return (
             <div>
                 {listaDePlaylists}
-                <button onClick={this.props.irParaLista}>Ir para lista</button>
+                <button onClick={this.props.irParaLista}>Ir para playlist</button>
                 
                 <Corpo>
                     <h1>Labefy</h1>
                     <h2>Playlist da labefy</h2>
-                    <h4>Digite o nome da musica ,url e autor</h4>
+                    <h4>Adicione nome da música, autor e url</h4>
                     <h2>Nome</h2>
                     <input
                         placeholder={"nome da musica"}
                         value={this.state.nome}
                         onChange={this.handleName}
                     />
+                    <h2>autor</h2>
+                    <input placeholder="autor da musica"
+                        value={this.state.autor}
+                        onChange={this.handleAutor}
+                    />
                     <h2>Url da musica</h2>
                     <input placeholder="url da musica"
                         value={this.state.urlDaMusica}
                         onChange={this.handleUrl}
                     />
-                    <h2>autor</h2>
-                    <input placeholder="autor da musica"
-                        value={this.state.autor}
-                        onChange={this.handlemusic}
-                    />
+                    
 
                     <button onClick={this.addTrackToPlaylist}>Inserir Musica</button>
 
