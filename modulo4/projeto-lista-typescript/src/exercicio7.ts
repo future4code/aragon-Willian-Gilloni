d13c105120d0c8a023ada65c02cfa45886a94c66
WillianGilloni <willianggvet@gmail.com>
2022-07-03 19:51:53 -0300

    Final dos exercicios

--- modulo4/projeto-lista-typescript/src/exercicio7.ts
+++ modulo4/projeto-lista-typescript/src/exercicio7.ts
@@ -28,14 +28,86 @@ const itens:Produtos[] = [
 
         return itensAtualizados
     })    
-        // function estoqueOrdenado (itensAtualizados:any):any {
-        //     const copiaLista = [...itensAtualizados]
-        //     const itensOrdenados:any = copiaLista.sort((a:any,b:any) => {
-        //         a.quantidade - b.quantidade
-        //         return itensOrdenados
-        //     })
-        // } 
+    function ordenaEstoque(itens:any) {
+        const itensComValorAtualizado = itens.map((item:any) => {
+            const valorAtualizado = item.valorUnitario.toFixed(2).replace(".", ",");
+            const itensAtualizados = {
+                nome: item.nome,
+                quantidade: item.quantidade,
+                valorUnitario: "R$" + valorAtualizado,
+            };
+            return itensAtualizados;
+        });
+        const compare = (a:any, b:any) => {
+            if (a.quantidade < b.quantidade)
+                return -1;
+            if (a.quantidade > b.quantidade)
+                return 1;
+            return 0;
+        };
+        return itensComValorAtualizado.sort(compare);
+    }
+    console.log(ordenaEstoque(itens));
     return ajustaPreco
 }
 
 console.log(retornaEstoqueOrdenado(itens))
+
+// RESPOSTA 2
+// const itens = [
+//     { nome: "MacMugffin", quantidade: 37, valorUnitario: 51.04 },
+//     { nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0 },
+//     { nome: "Laço da verdade", quantidade: 32, valorUnitario: 571.5 },
+//     { nome: "O precioso", quantidade: 1, valorUnitario: 9181.923 },
+//     { nome: "Caneta de 250 cores", quantidade: 123, valorUnitario: 17 },
+//     { nome: "Plumbus", quantidade: 13, valorUnitario: 140.44 },
+//     { nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915 },
+// ];
+// function ordenaEstoque(itens) {
+//     const itensComValorAtualizado = itens.map((item) => {
+//         const valorAtualizado = item.valorUnitario.toFixed(2).replace(".", ",");
+//         const itensAtualizados = {
+//             nome: item.nome,
+//             quantidade: item.quantidade,
+//             valorUnitario: "R$" + valorAtualizado,
+//         };
+//         return itensAtualizados;
+//     });
+//     const resultado = itensComValorAtualizado.sort((a, b) => {
+//         if (a.quantidade < b.quantidade)
+//             return -1;
+//         if (a.quantidade > b.quantidade)
+//             return 1;
+//     })
+
+//     return resultado;
+// }
+// console.log(ordenaEstoque(itens));
+
+// RESPOSTA 3
+// const itens = [
+//     { nome: "MacMugffin", quantidade: 37, valorUnitario: 51.04 },
+//     { nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0 },
+//     { nome: "Laço da verdade", quantidade: 32, valorUnitario: 571.5 },
+//     { nome: "O precioso", quantidade: 1, valorUnitario: 9181.923 },
+//     { nome: "Caneta de 250 cores", quantidade: 123, valorUnitario: 17 },
+//     { nome: "Plumbus", quantidade: 13, valorUnitario: 140.44 },
+//     { nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915 },
+// ];
+// function ordenaEstoque(itens) {
+//     const itensComValorAtualizado = itens.map((item) => {
+//         const valorAtualizado = item.valorUnitario.toFixed(2).replace(".", ",");
+//         const itensAtualizados = {
+//             nome: item.nome,
+//             quantidade: item.quantidade,
+//             valorUnitario: "R$" + valorAtualizado,
+//         };
+//         return itensAtualizados;
+//     });
+//     const resultado = itensComValorAtualizado.sort((a, b) => {
+//         return a.quantidade < b.quantidade ? -1 : a.quantidade > b.quantidade ? 1 : 0 // mudei apenas essa parte
+//     })
+
+//     return resultado;
+// }
+// console.log(ordenaEstoque(itens));
