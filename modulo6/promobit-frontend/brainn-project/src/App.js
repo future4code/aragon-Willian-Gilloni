3d9d8b2904059f885fd55f65022e1122e86f2be5
WillianGilloni <willianggvet@gmail.com>
2022-08-29 18:36:28 -0300

    Adicionar brainn project

---
+++ modulo6/promobit-frontend/brainn-project/src/App.js
@@ -0,0 +1,11 @@
+import { Router } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo6/promobit-frontend/brainn-project/src/routes/Router.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+
+
+function Router ()  {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+                <Route index element={ <Home /> } />
+
+            </Routes>
+        </BrowserRouter>
+    );
+}; 
+
+export default Router
